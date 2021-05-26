014e0e4a4a8bc57faf29c9fe6d0400d278084b1b
Isabelle <alvesisabele370@gamil.com>
2021-05-26 09:29:14 -0300

    Initial commit

---
+++ app/src/main/java/com/example/aula_26_05/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.example.aula_26_05
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    //Isabelle Alves Dutra
+    
+}
